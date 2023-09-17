b4623063721af0d75fb8c570df82f0a387faf975
Rabyn-Collab <babynshrestha64@gmail.com>
2023-09-17 17:03:25 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:riverspods/view/home_page.dart';
-import 'package:riverspods/view/sample_page.dart';
-import 'package:riverspods/view/sample_page_1.dart';
-
 
 
 
 void main (){
-
 runApp(Home());
-
 
 }
 
@@ -19,10 +13,9 @@ runApp(Home());
 
 class Home extends StatelessWidget {
 
-
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+
     return ScreenUtilInit(
       designSize: const Size(375, 812),
       minTextAdapt: true,
@@ -38,9 +31,7 @@ class Home extends StatelessWidget {
           //   color: Colors.amber
           // )
         ),
-        home: MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaleFactor: width > 550 ? 0.9: 1.0),
-            child: SamplePage1()),
+        home : Container()
       ),
     );
   }
