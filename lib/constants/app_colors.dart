70fed17b9f72b6697fe587bd55cb8a28234d86fa
Rabyn-Collab <babynshrestha64@gmail.com>
2023-09-20 17:41:38 +0545

    changes

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 class AppColors{
 
  static const appWhiteColor = Color(0xFFF2F5F9);
+ static const appMainColor = Colors.blueGrey;
 
 
 }
--- lib/view/todo_page.dart
+++ lib/view/todo_page.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverspods/common_widget/custom_button.dart';
 import 'package:riverspods/constants/app_sizes.dart';
 import 'package:riverspods/models/todo.dart';
 import 'package:riverspods/providers/todo_provider.dart';
 
-
 class TodoPage extends ConsumerWidget {
-   TodoPage({super.key});
+  TodoPage({super.key});
 
   final todoController = TextEditingController();
   @override
   Widget build(BuildContext context, ref) {
     final state = ref.watch(todoProvider);
     return Scaffold(
-          appBar: AppBar(
-            title: Text('Todo Notifier'),
-          ),
+        appBar: AppBar(
+          title: Text('Todo Notifier'),
+        ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: Column(
@@ -23,38 +23,47 @@ class TodoPage extends ConsumerWidget {
               TextFormField(
                 controller: todoController,
                 decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                   hintText: 'add some todo',
                   border: OutlineInputBorder(),
                 ),
-                onFieldSubmitted: (val){
-                    if(val.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          duration: Duration(seconds: 1),
-                            content: Text('please provide todo task'))
-                    );
-                    }else{
-                      ref.read(todoProvider.notifier).addTodo(Todo(
-                          created_at: DateTime.now().toString(),
-                          todo: val.trim()
-                      ));
-                      todoController.clear();
-                    }
-
+                onFieldSubmitted: (val) {
+                  if (val.isEmpty) {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        duration: Duration(seconds: 1),
+                        content: Text('please provide todo task')));
+                  } else {
+                    ref.read(todoProvider.notifier).addTodo(Todo(
+                        created_at: DateTime.now().toString(),
+                        todo: val.trim()));
+                    todoController.clear();
+                  }
                 },
               ),
 
-              AppSizes.gapH16,
+              // AppSizes.gapH16,
+              // CustomButton(
+              //     label: 'Login',
+              //     onPressed: () {
+              //       print('login');
+              //     }),
+              // CustomButton(
+              //     label: 'SignUp',
+              //     onPressed: () {
+              //       print('register');
+              //     }),
+              // _buildRow(label: 'sldkjfslkdfjfsdk', iconData: Icons.accessibility_sharp),
+              // _buildRow(label: 'jruuwwoewp', iconData: Icons.ac_unit_sharp),
 
               Expanded(
                   child: ListView.separated(
-                    separatorBuilder: (c, i){
-                      return Divider();
-                    },
-                       itemCount: state.length,
-                      itemBuilder: (context, index){
-                         final todo = state[index];
+                      separatorBuilder: (c, i) {
+                        return Divider();
+                      },
+                      itemCount: state.length,
+                      itemBuilder: (context, index) {
+                        final todo = state[index];
                         return ListTile(
                           contentPadding: EdgeInsets.zero,
                           leading: Icon(Icons.date_range),
@@ -65,41 +74,80 @@ class TodoPage extends ConsumerWidget {
                               child: Row(
                                 children: [
                                   IconButton(
-                                      onPressed: (){},
-                                      icon: Icon(Icons.edit, color: Colors.green,)
-                                  ),
-                                  IconButton(
-                                      onPressed: (){
+                                      onPressed: () {
+                                        buildShowDialog(
+                                            context: context,
+                                            isUpdate: true,
+                                            todo: todo,
+                                            onPressed: (){
+                                              Navigator.of(context).pop();
 
-                                       showDialog(context: context, builder: (context){
-                                         return AlertDialog(
-                                           title: Text('Hold On'),
-                                           content: Text('Are you sure ?'),
-                                           actions: [
-                                             TextButton(onPressed: (){
-                                               Navigator.of(context).pop();
-                                               ref.read(todoProvider.notifier).removeTdo(todo);
-                                             }, child: Text('sure')),
-                                             TextButton(onPressed: (){
-                                               Navigator.of(context).pop();
-                                             }, child: Text('not sure')),
-                                           ],
-                                         );
-                                       });
-
+                                            }
+                                        );
                                       },
-                                      icon: Icon(Icons.delete, color: Colors.pink,)
-                                  ),
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.green,
+                                      )),
+                                  IconButton(
+                                      onPressed: () {
+                                        buildShowDialog(
+                                          context: context,
+                                          isUpdate: false,
+                                          todo: todo,
+                                          onPressed: (){
+                                            Navigator.of(context).pop();
+                                            ref.read(todoProvider.notifier).removeTdo(todo);
+                                          }
+                                        );
+                                      },
+                                      icon: Icon(
+                                        Icons.delete,
+                                        color: Colors.pink,
+                                      )),
                                 ],
-                              )
-                          ),
+                              )),
                         );
-                      }
-                  )
-              )
+                      }))
             ],
           ),
-        )
+        ));
+  }
+
+  Future<dynamic> buildShowDialog({
+   required BuildContext context,
+  required VoidCallback onPressed,
+   required bool isUpdate,
+    required Todo todo
+  }) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(isUpdate ? 'Update Post': 'Hold On'),
+            content: isUpdate ? TextFormField(
+               initialValue: todo.todo,
+            ) :Text('Are you sure ?'),
+            actions: [
+              TextButton(
+                  onPressed: onPressed,
+                  child: Text('sure')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('not sure')),
+            ],
+          );
+        });
+  }
+
+  Row _buildRow({required String label, required IconData iconData}) {
+    return Row(
+      children: [
+        Text(label),
+        IconButton(onPressed: () {}, icon: Icon(iconData)),
+      ],
     );
   }
 }
